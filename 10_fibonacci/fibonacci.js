9b95a18677af9c3fd73ce7119e81ac58120f12dc
Julian <julian_wv@live.com.ar>
2022-05-07 17:58:25 -0300

    fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(userVal) {
+    let lastValue = 0;
+    let nextValue = 1;
+    let currentValue = 0;
+    let fibonacciArray = [];
 
+    for(let x = 0; x <= userVal; x++){
+        fibonacciArray.push(currentValue);
+        currentValue = lastValue + nextValue;
+        nextValue = lastValue;
+        lastValue = currentValue;
+    }
+    return fibonacciArray[fibonacciArray.length - 1];
 };
 
 // Do not edit below this line
